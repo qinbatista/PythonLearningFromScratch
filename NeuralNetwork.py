f1278139ebb43a28bf3145d5c804facbd472f5d8
qin yu peng <qin.batista@outlook.com>
2019-12-04 15:09:39 +0800

    更新神经网络代码

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -258,7 +258,6 @@ t = np.array([0,0,1])
 net.loss(x,t)
 
 # %%
-
 def numerical_gradient(f, x):
 	h = 1e-4 # 0.0001
 	grad = np.zeros_like(x)
@@ -278,4 +277,40 @@ f = lambda w: net.loss(x, t)
 dw = numerical_gradient(f, net.W)
 print(dw)
 
-# %%
+# %% 2层神经网络的类
+class TwoLayerNet:
+	def __init__(self, input_size, hidden_size, outout_size, weight_init_std =0.01):
+		self.params = {}
+		self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+		self.params['b1'] = np.zeros(hidden_size)
+		self.params['W2'] = weight_init_std * np.random.randn(hidden_size , outout_size)
+		self.params['b2'] = np.zeros(outout_size)
+
+	def predict(self, x):
+		W1, W2 = self.params['W1'], self.params['W2']
+		b1, b2 = self.params['b1'], self.params['b2']
+		a1 = np.dot(x, W1) +b1
+		z1 = sigmoid(a1)
+		a2 = np.dot(z1,W2) + b2
+		y = softmax(a2)
+		return y
+
+	def loss(self, x, t):
+		y = self.predict(x)
+		return cross_entropy_error(y , t)
+
+	def accuracy(self, x, t):
+		y = self.predict(x)
+		y = np.argmax(y, axis=1)
+		t = np.argmax(t, axis=1)
+		accuracy = np.sum(y == t) / float(x.shape[0])
+		return accuracy
+
+	def numerical_gradient(self, x, t):
+		loss_W = lambda W: self.loss(x,t)
+		grads = {}
+		grads['W1'] = numerical_gradient(loss_W , self.params['W1'])
+		grads['b1'] = numerical_gradient(loss_W , self.params['b1'])
+		grads['W2'] = numerical_gradient(loss_W , self.params['W2'])
+		grads['b2'] = numerical_gradient(loss_W , self.params['b2'])
+		return grads
